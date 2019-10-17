bbf8b3c61fb837a0bf3ee3d11de35df1c3b6848f
BesfortYmaj <38860009+BesfortYmaj@users.noreply.github.com>
2019-10-17 11:36:12 -0400

    Files for register feature
    files for the register feature that will help users create accounts.

---
+++ signup.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+
+$username = "";
+$errors = array(); 
+
+// connect to the database need team's DB
+$db = mysqli_connect('localhost', 'root', '', 'registration');
+
+
+if (isset($_POST['reg_user'])) {
+
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  
+  $user_check_query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { 
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+   
+  }
+
+  
+  if (count($errors) == 0) {
+  	$password = md5($password_1);
+
+  	$query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$username', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+?>
