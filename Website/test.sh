335db97411f0c0559fae24d2dae2b4018a65c88a
devonzhen22 <dz89@njit.edu>
2019-12-14 13:53:35 -0500

    deletion

--- Website/test.sh
+++
@@ -1,4 +0,0 @@
-#scp <source> <destination>
-#scp /path/to/file username@:/path/to/destination
-scp /home/devon/Documents/tet.zip texmex@192.168.1.103:/var/www/html
-#scp home/devon/Documents/testwork.zip johane@192.168.1.101:/var/www/html
