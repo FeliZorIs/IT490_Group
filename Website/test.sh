d9a8f01ad4194fc0f4e75e7f48f45a96de2743f8
devonzhen22 <dz89@njit.edu>
2019-12-14 13:50:17 -0500

    test.sh

---
+++ Website/test.sh
@@ -0,0 +1,4 @@
+#scp <source> <destination>
+#scp /path/to/file username@:/path/to/destination
+scp /home/devon/Documents/tet.zip texmex@192.168.1.103:/var/www/html
+#scp home/devon/Documents/testwork.zip johane@192.168.1.101:/var/www/html
